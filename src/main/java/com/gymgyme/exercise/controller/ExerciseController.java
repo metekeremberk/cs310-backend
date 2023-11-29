e70d8fee68b7d5ec5ddaea9d897ce170ea9778e3
Mete Kerem Berk <metekeremberk@gmail.com>
2023-11-29 17:31:16 +0300

    Search deleted

--- src/main/java/com/gymgyme/exercise/controller/ExerciseController.java
+++ src/main/java/com/gymgyme/exercise/controller/ExerciseController.java
@@ -6,16 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gymgyme.exercise.model.BestExercise;
 import com.gymgyme.exercise.model.Exercise;
 import com.gymgyme.exercise.repository.best_exercise.BestExerciseRepository;
-import com.gymgyme.exercise.repository.best_exercise.SearchBestRepository;
 import com.gymgyme.exercise.repository.exercise.ExerciseRepository;
-import com.gymgyme.exercise.repository.exercise.SearchRepository;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletResponse;
@@ -28,13 +25,7 @@ public class ExerciseController {
 	ExerciseRepository exerciseRepo;
 	
 	@Autowired
-	SearchRepository searchRepo;
-	
-	@Autowired
 	BestExerciseRepository bestRepo;
-	
-	@Autowired
-	SearchBestRepository searchBestRepo;
 	
 	@Hidden
 	@RequestMapping(value="/")
@@ -47,18 +38,8 @@ public class ExerciseController {
 		return exerciseRepo.findAll(); 
 	}
 	
-	@GetMapping(value="/exercises/{text}&{limit}")
-	public List<Exercise> searchExercises(@PathVariable String text, @PathVariable int limit) {
-		return searchRepo.findByName(text, limit);
-	}
-	
 	@GetMapping(value="/best")
 	public List<BestExercise> getBestExercises() {
 		return bestRepo.findAll(); 
-	}
-	
-	@GetMapping(value="/best/{text}")
-	public List<BestExercise> searchBestExercises(@PathVariable String text) {
-		return searchBestRepo.findByText(text);
 	}
 }
--- src/main/java/com/gymgyme/exercise/repository/best_exercise/SearchBestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gymgyme.exercise.repository.best_exercise;
-
-import java.util.List;
-
-import com.gymgyme.exercise.model.BestExercise;
-
-public interface SearchBestRepository {
-	List<BestExercise> findByText(String text);
-}
